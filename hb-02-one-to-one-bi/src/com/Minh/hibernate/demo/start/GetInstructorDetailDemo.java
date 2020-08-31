b744516da776e4359b88aabcbb724f8ac9bafe21
Minh <minh@macs-MacBook-Pro.local>
2020-08-31 23:45:43 +0700

    Create relationship between instructor and courses and deploy data to database

---
+++ hb-02-one-to-one-bi/src/com/Minh/hibernate/demo/start/GetInstructorDetailDemo.java
@@ -0,0 +1,53 @@
+package com.Minh.hibernate.demo.start;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.Minh.hibernate.demo.entity.Instructor;
+import com.Minh.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+//		create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Instructor.class)
+				.buildSessionFactory();
+				
+//		create session
+		Session session = factory.getCurrentSession();
+		
+		try {					
+			session.beginTransaction();
+			
+//			Get instructor detail object
+			int theId = 100;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			
+//			Print instructor detail
+			System.out.println("My instructor detail: " + tempInstructorDetail);
+			
+//			Print associated
+			System.out.println("Instructor in my instructor detail: " + tempInstructorDetail.getInstructor());
+			
+//			commit trasaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			
+			session.close();
+			factory.close();
+		}
+		
+	}
+
+}
